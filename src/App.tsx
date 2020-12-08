a1e33793e3a114e92313b7c3112a7527bf44e1e8
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-08 11:02:04 -0300

    Added AuthProvider as Component Parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from "./pages/login";
+import SignUpPage from "./pages/signup";
+import PrivateRoute from "./PrivateRoutes";
+import HomePage from "./pages/home";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./globals.css";
+
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={LoginPage}/>
-            </Switch>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Switch>
+                    <PrivateRoute exact path="/home" component={HomePage}/>
+                    <Route exact path="/" component={LoginPage}/>
+                    <Route path="/signup" component={SignUpPage}/>
+                </Switch>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
