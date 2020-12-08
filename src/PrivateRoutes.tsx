afb77dd2263c6a766184eee4cfda42a5d634cd8e
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-08 11:00:56 -0300

    PrivatedRoute Created

---
+++ src/PrivateRoutes.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./contexts/AuthContext";
+
+const PrivateRoute = ({ RouteComponent, ...rest}: any) => {
+    const {authenticated, loadingAuthState} = useContext(AuthContext);
+    console.log(loadingAuthState);
+    if (loadingAuthState) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+     );
+  }
+   return (
+      <Route
+        {...rest}
+        render={routeProps =>
+        authenticated ? (
+         <RouteComponent {...routeProps} />
+        ) : (
+         <Redirect to={{pathname: "/", state: {prevPath:  rest.path}}} />
+       )
+      }
+     />
+    );
+  }
+  export default PrivateRoute
