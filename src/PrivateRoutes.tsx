2c9663572990a6d487eeddfe9b361c8f055fc39a
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-16 12:48:15 -0300

    User can't go to the HomePage without being logged

--- src/PrivateRoutes.tsx
+++ src/PrivateRoutes.tsx
@@ -2,26 +2,22 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./contexts/AuthContext";
 
-const PrivateRoute = ({ RouteComponent, ...rest}: any) => {
-  const {authenticated, loadingAuthState} = useContext(AuthContext);
-  if (loadingAuthState) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-  return (
-      <Route
-        {...rest}
-        render={routeProps =>
-        authenticated ? (
-          <RouteComponent {...routeProps} />
-        ) : (
-          <Redirect to={{pathname: "/", state: {prevPath:  rest.path}}} />
-        )
-      }
-      />
-    );
-  }
+const PrivateRoute = ({Component, ...rest}: any) => {
+	const {authenticated, loadingAuthState} = useContext(AuthContext);
+	console.log(authenticated);
+	if (loadingAuthState) {
+		return (
+			<div>
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	if (!authenticated)
+		return  <Redirect to="/"/>
+	return (
+		<Route {...rest} render={props => { <Component {...props}/>}}/>
+	);
+}
+
 export default PrivateRoute
