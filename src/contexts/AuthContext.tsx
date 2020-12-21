53af737bbd1db3c44cd66570cc4736803be918af
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-21 14:07:47 -0300

    Added Email to the authContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,27 +4,31 @@ import app from "../services/firebase";
 
 type ContextProps = {
     user: firebase.User | null;
+    email: firebase.UserInfo | null;
     authenticated: boolean;
     setUser: any;
+    setEmail: any;
     loadingAuthState: boolean;
 };
 
 export const AuthContext =  React.createContext<Partial<ContextProps>>({});
 export const AuthProvider = ({ children }: any) => {
     const [user, setUser] = useState(null as firebase.User | null);
+    const [email, setEmail] = useState(null as firebase.UserInfo | null);
     const [loadingAuthState, setLoadingAuthState] = useState(true);
     useEffect(() => {
             app.auth().onAuthStateChanged((user: any) => {
             setUser(user);
+            setEmail(user.email);
             setLoadingAuthState(false);
         });
     }, []);
-
 
     return (
         <AuthContext.Provider
         value={{
             user,
+            email,
             authenticated: user !== null,
             setUser,
             loadingAuthState
