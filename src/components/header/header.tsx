316204cf3c5672c5ef4d3bd5d296972d671fa9ac
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-22 14:53:41 -0300

    Header was componentized

---
+++ src/components/header/header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import "./header.css";
+
+interface IThemeValue{
+    isThemeDark: boolean; 
+    setIsThemeDark(state: boolean): void;
+}
+
+interface ImenuActivate {
+    menuActivate: boolean;
+    setMenuActivate(state: boolean): void;
+}
+
+export default function Header({menuValue, theme}: any){
+    let {menuActivate, setMenuActivate}: ImenuActivate = menuValue;
+    let {isThemeDark, setIsThemeDark}: IThemeValue = theme;
+    return (
+        <>
+            <section className="app-bar">
+                <div className="left-section">
+                    <i className="fas fa-bars" onClick={e => setMenuActivate(!menuActivate)}></i>
+                    <Logo />
+                </div>
+
+                <h3>Today</h3>
+                <div className="theme-switch">
+                    <h4>Light</h4>
+                    <label className="switch">
+                        <input checked={isThemeDark} type="checkbox" onClick={e =>{localStorage.setItem("theme", (!isThemeDark).toString());setIsThemeDark(!isThemeDark)}}/>
+                        <span className="slider"></span>
+                    </label>
+                    <h4>Dark</h4>
+                </div>
+            </section>
+        </>
+    );
+}
