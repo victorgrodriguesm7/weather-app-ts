146f12c9ed797e9dc74d31e1af7b2b9587880997
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-22 14:55:57 -0300

    Header & NavMenu are now together in the Dashboard

---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import app from "../../services/firebase";
+import NavMenu from "../navmenu/navmenu";
+import Header from "../header/header";
+
+interface IThemeValue{
+    isThemeDark: boolean; 
+    setIsThemeDark(state: boolean): void;
+}
+
+export default function Dashboard({theme}: any){
+    const location = useLocation().pathname;
+    const history = useHistory();
+    let {isThemeDark, setIsThemeDark}: IThemeValue = theme;
+    let [itemNavMenuSelected, setItemNavMenuSelected] = useState(location === "/home");
+    let [menuActivate, setMenuActivate] = useState(false);
+    function changeItem(e: React.MouseEvent<any, globalThis.MouseEvent>, state: boolean){
+        setItemNavMenuSelected(state);
+        localStorage.setItem("itemNavMenuSelected", state.toString());
+    }
+    
+    function handleLogout(e: React.MouseEvent<any, globalThis.MouseEvent>){
+        app.auth().signOut()
+        .then(() => {
+            history.push("/");
+        }).catch(error => {
+            console.log(error);
+        });         
+    }
+
+    return (
+        <>
+            <NavMenu 
+                itemNavMenu={{itemNavMenuSelected, setItemNavMenuSelected}}
+                menuValue={{menuActivate, setMenuActivate}}
+                changeItemFunction={changeItem}
+                handleLogoutFunction={handleLogout}/>
+            <Header
+                menuValue={{menuActivate, setMenuActivate}}
+                theme={{isThemeDark, setIsThemeDark}}/>
+        </>
+    );
+}
