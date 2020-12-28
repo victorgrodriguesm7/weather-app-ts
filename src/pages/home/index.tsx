f17d06d787f775725043ff037f9d7d27d5c1d32d
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-12-27 22:13:36 -0300

    Removed Css file

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AddCard from "../../components/addcard/addCard";
 import Dashboard from "../../components/dashboard/dashboard";
 import ThemeSwitch from "../../components/theme/theme";
-import "./index.css";
 
 export default function HomePage() {
     let {isThemeDark, setIsThemeDark, darkTheme} = ThemeSwitch();
