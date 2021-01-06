ab1c891ac967d1095b634c4a286b5e4da20dd51d
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-06 13:09:22 -0300

    Added Props, now you can pass the row and column

--- src/components/addcard/addCard.tsx
+++ src/components/addcard/addCard.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { ReactComponent as Cityilustrationlight } from '../../assets/city-ilustration-light.svg';
 import './addCard.css';
-
-export default function AddCard(){
+interface AddCardProps {
+    row?: number;
+    column?: string;
+}
+export default function AddCard({ row = 2, column="2"}: AddCardProps){
     const history = useHistory();
     return (
         <div className="add-card"
-            onClick={()=> history.push("/add")}>
+            onClick={()=> history.push("/add")}
+            style={{ gridRow: row, gridColumn: column}}>
             <div className="icon-button">
                 <p>ADD CITY</p>
                 <i className="add-icon fas fa-plus"></i>
